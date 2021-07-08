7edaf6a1abf1697b82a2ee38491f93dc9d891713
”kensuke” <kensuke1202left@icloud.com>
2021-07-08 15:06:38 +0900

    add docker-compose up command

--- scripts/change_permission.sh
+++ scripts/change_permission.sh
@@ -3,3 +3,5 @@
 sudo chmod 755 /var/app/scripts/start.sh
 
 docker network create ALLHOME-PROJECT-NETWORK
+
+docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml up -d
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker network create ALLHOME-PROJECT-NETWORK
 
 docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml down
 
