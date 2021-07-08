6587f7a310fb2188947c18573d3a041bd43936c2
”kensuke” <kensuke1202left@icloud.com>
2021-07-08 14:56:15 +0900

    coment out to docker-compose up and add docker network create

--- scripts/change_permission.sh
+++ scripts/change_permission.sh
@@ -2,4 +2,6 @@
 
 sudo chmod 755 /var/app/scripts/start.sh
 
-docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml up -d
+docker network create ALLHOME-PROJECT-NETWORK
+
+# docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml up -d
