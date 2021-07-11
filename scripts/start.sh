1b71adcb3efb97429fcb2036032467e8df8ab4dd
”kensuke” <kensuke1202left@icloud.com>
2021-07-11 12:25:03 +0900

    scriptの修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-docker network create ALLHOME-PROJECT-NETWORK
+sudo yum update -y
+echo y | sudo yum install docker
+echo y | sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker
 
-docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml down
+sudo docker network create ALLHOME-PROJECT-NETWORK
 
-docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml up -d
+sudo docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml down
+
+sudo docker-compose -p mytrainer-app-dbserver -f /var/app/docker-compose.yml up -d
